1feff4d2f0dd1f7ff7076335a5ed907707089bbd
YourName <you@example.com>
2020-04-02 17:33:29 +0900

    create card registration view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "users#show"
+  root "users#index"
+  resources :cards, only: [:new, :show] do
+    collection do
+      post 'show', to: 'cards#show'
+      post 'pay', to: 'cards#pay'
+      post 'delete', to: 'cards#delete'
+    end
+  end
 end
