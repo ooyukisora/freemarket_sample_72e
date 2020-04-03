e2bc9832e19400035b73a0e0faa9d6934e498f3a
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-04-03 16:17:06 +0900

    Update forms for delivery_fee

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
-  
+  belongs_to_active_hash :burden
+
   belongs_to :user
 end
--- db/migrate/20200402094819_create_items.rb
+++ db/migrate/20200402094819_create_items.rb
@@ -13,6 +13,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.references :user
       t.integer :prefecture_id
       t.integer :condition_id
+      t.integer :burden_id
       # t.references :category
       # t.references :address
       # t.references :image
