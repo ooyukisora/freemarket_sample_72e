b1036c0dfd26291b83c2b9acf1f6aac6b338a5e5
ooyukisora <ooyukisora@gmail.com>
2020-04-03 21:48:24 +0900

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
+
+
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
+    get '/users/sign_out' => 'users/registrations#destroy'
   end
   root "users#index"
+  resources :users, only: :show
   resources :cards, only: [:new, :show] do
     collection do
       post 'show', to: 'cards#show'
