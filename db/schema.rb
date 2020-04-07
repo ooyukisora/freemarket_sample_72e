8751b865bce22bb3a33bda78cedf067a13de6756
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-04-07 16:32:08 +0900

    update test

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_023141) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "img"
+    t.string "img", null: false
     t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
