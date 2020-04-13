b05d0b7e67cd34f185e4967c58c78063b43d66ae
YourName <you@example.com>
2020-04-13 15:26:16 +0900

    update details

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :burden
   belongs_to_active_hash :shipday
   belongs_to :user
+  belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
