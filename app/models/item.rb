06db881be803f6d1dc997aaf3926dcc5a0575194
YourName <you@example.com>
2020-04-14 14:55:58 +0900

    create comment

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :images, dependent: :destroy
+  has_many :comments
   accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :name, :price, :text, :status, 
