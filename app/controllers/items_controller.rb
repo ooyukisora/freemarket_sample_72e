5ee334cc7f58398f1c6fa51a7137578bde198511
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-04-14 18:19:42 +0900

    Create basic edit function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only:[:show, :edit, :destroy]
+  before_action :set_item, only:[:show, :edit, :destroy, :update]
 
   def index
   end
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new
-    @category_parent_array = ["---"]
+    @category_parent_array = []
       Category.where(ancestry: nil).each do |parent|
         @category_parent_array << parent.name
     end
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      redirect_to root_path
+      render :new
     end
   end
   
@@ -51,8 +51,27 @@ class ItemsController < ApplicationController
     end
   end
 
+
   def edit
     
+    grandchild_category = @item.category
+    child_category = grandchild_category.parent
+
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    @category_children_array = []
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_children_array << children
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
+
   end
 
   def update
@@ -72,7 +91,7 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :text, :price, 
       :category_id, :status, :delivery_fee, :shipping_day, 
-      :from_area, images_attributes: [:img]).merge(user_id: current_user.id)
+      :from_area, images_attributes: [:img, :_destroy, :id]).merge(user_id: current_user.id)
   end
 
   def set_item
