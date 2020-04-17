b1ac8f1ba8b240b8ec597429f38c2ad05d9d02f1
ooyukisora <ooyukisora@gmail.com>
2020-04-17 11:44:44 +0900

    update capistrano v

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,5 +13,5 @@ class Item < ApplicationRecord
   validates :name, :price, :text, :status, 
     :delivery_fee, :from_area, :shipping_day, :category_id, presence: true
   
-  # validates :images, presence: true
+  validates :images, presence: true
 end
